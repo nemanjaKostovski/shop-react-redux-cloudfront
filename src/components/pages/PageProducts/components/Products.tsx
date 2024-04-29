054448e9dc4b0a8e4a8200352415fa4e5f0a3dea
Nemanja <nemanjakostovski@gmail.com>
2024-04-29 02:15:27 +0200

    added fix for DynamoDB in FE

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,15 +1,18 @@
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { formatAsPrice } from "~/utils/utils";
-import AddProductToCart from "~/components/AddProductToCart/AddProductToCart";
-import { useAvailableProducts } from "~/queries/products";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { formatAsPrice } from '~/utils/utils';
+import AddProductToCart from '~/components/AddProductToCart/AddProductToCart';
+import { useAvailableProducts } from '~/queries/products';
+import { fixDBData } from '~/utils/utils';
 
 export default function Products() {
   const { data = [], isLoading } = useAvailableProducts();
+  // Added custom function to fix DynamoDB problem
+  const fixedData = fixDBData(data);
 
   if (isLoading) {
     return <Typography>Loading...</Typography>;
@@ -18,18 +21,18 @@ export default function Products() {
   return (
     <Grid container spacing={4}>
       {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4}>
+      {fixedData.map(({ count, ...product }, index) => (
+        <Grid item key={product.productId} xs={12} sm={6} md={4}>
           <Card
-            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
           >
             <CardMedia
-              sx={{ pt: "56.25%" }}
+              sx={{ pt: '56.25%' }}
               image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
+              title='Image title'
             />
             <CardContent sx={{ flexGrow: 1 }}>
-              <Typography gutterBottom variant="h5" component="h2">
+              <Typography gutterBottom variant='h5' component='h2'>
                 {product.title}
               </Typography>
               <Typography>{formatAsPrice(product.price)}</Typography>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,30 @@
-const priceFormatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
+const priceFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
 });
 
 export const formatAsPrice = (price: number) => priceFormatter.format(price);
+
+// My tryout to fix issue with DynamoDB structure
+interface OriginalItem {
+  description: { S: string };
+  price: { N: number };
+  productId: { S: string };
+  title: { S: string };
+}
+
+interface TransformedItem {
+  description: string;
+  price: number;
+  productId: string;
+  title: string;
+}
+
+export function fixDBData(originalData: OriginalItem[]): TransformedItem[] {
+  return originalData.map((item) => ({
+    description: item.description.S,
+    price: item.price.N,
+    productId: item.productId.S,
+    title: item.title.S,
+  }));
+}
